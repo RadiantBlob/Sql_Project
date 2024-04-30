43c0e0a27b2cd813825468df25543c140bd3af18
radiantFelix <mullerfelix178@gmai.com>
2024-04-30 17:03:24 +0200

    updated readmy

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+import sqlite3
+# from flask import Flask
+# 
+# app = Flask(__name__)
+# 
+class User:
+    def __init__(self, username, firstname, lastname):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
 
-app = Flask(__name__)
+    def to_db(self):
+        pass
 
-@app.route("/")
-def index():
-	return "Hello, World!"
+    @classmethod
+    def from_db(cls):
+        connection = sqlite3.connect("poke.db") # Muss vorher angelegt werden.
+        cursor = connection.cursor()
+        sql = f"SELECT * "
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        connection.close()
+        return User(row[0], row[1], row[2])
 
-app.run()
+user = User.from_db()
