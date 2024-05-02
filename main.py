7aaaef4db010d8126356c64c72ca004ac979f1f3
Felix Müller <mullerfelix178@gmail.com>
2024-05-02 18:19:09 +0200

    added the ability to load Pokemon from file and write to the file

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
 import sqlite3
-# from flask import Flask
-# 
-# app = Flask(__name__)
-# 
-class User:
-    def __init__(self, username, firstname, lastname):
-        self.username = username
-        self.firstname = firstname
-        self.lastname = lastname
 
-    def to_db(self):
-        pass
+class Pokemon:
+	def __init__(
+			self, num: int, name: str, type1: str, type2: str, gen: int, abilities: str, hp: int, speed: int,
+			attack: int, defense: int, classification: str, height: int, weight: int, is_leg: bool):
+		self.pokedex_number: int = num
+		self.name: str = name
+		self.type1: str = type1
+		self.type2: str = type2
+		self.generation: int = gen
+		self.abilities: str = "I DO NOT CARE"
+		self.hp: int = hp
+		self.speed: int = speed
+		self.attack: int = attack
+		self.defense: int = defense
+		self.classification: str = classification
+		self.height: int = height
+		self.weight: int = weight
+		self.is_legendary: bool = is_leg
 
-    @classmethod
-    def from_db(cls):
-        connection = sqlite3.connect("poke.db") # Muss vorher angelegt werden.
-        cursor = connection.cursor()
-        sql = f"SELECT * "
-        cursor.execute(sql)
-        row = cursor.fetchone()
-        connection.close()
-        return User(row[0], row[1], row[2])
+	@classmethod
+	def from_db(cls, index: int):
+		con = sqlite3.connect("poke.db")
+		cursor = con.cursor()
+		sql = f"Select * from pokemon where pokedex_number = {index}"
+		cursor.execute(sql)
+		row = cursor.fetchone()
+		con.close()
+		print(row)
+		return cls(*row)
 
-user = User.from_db()
+	# def to_db(self):
+	#     con = sqlite3.connect("poke.db")
+	#     cursor = con.cursor()
+	#     sql = (f"INSERT INTO pokemon ('pokedex_number', 'name', 'type1', 'type2', 'generation', 'abilities', 'hp', "
+	#            f"'speed','attack', 'defense', 'classfication', 'height_m', 'weight_kg', 'is_legendary') VALUES "
+	#            f"('{self.pokedex_number}', '{self.name}', '{self.type1}', '{self.type2}', '{self.generation}', "
+	#            f"'{self.abilities}', '{self.hp}', '{self.speed}', '{self.attack}', '{self.defense}',"
+	#            f"'{self.classification}', '{self.height}', '{self.weight}', '{self.is_legendary}');")
+	#     cursor.execute(sql)
+	#     con.commit()
+	#     con.close()
+
+	def to_db(self):
+		connection = sqlite3.connect("poke.db")  # Muss vorher angelegt werden.
+		cursor = connection.cursor()
+		sql = (
+			f"INSERT INTO pokemon(pokedex_number, name, type1, type2, generation, abilities, hp, speed, attack, defense, classfication, height_m, weight_kg, is_legendary) VALUES "
+			f"('{self.pokedex_number}', '{self.name}', '{self.type1}', '{self.type2}', '{self.generation}', '{self.abilities}', '{self.hp}', '{self.speed}', '{self.attack}', '{self.defense}', '{self.classification}', '{self.height}', '{self.weight}', '{self.is_legendary}')")
+		cursor.execute(sql)
+		connection.commit()
+		connection.close()
+
+
