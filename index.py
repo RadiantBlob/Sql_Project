49b889ae4786719355935cd0c37a02b53c25a1d6
Felix Müller <mullerfelix178@gmail.com>
2024-05-02 23:34:24 +0200

    index python file for flask, currently shows all pokemon

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from backend import Pokemon, return_all
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("home.html",
+	                       lable=vars(n :=Pokemon.from_db(1)).keys(),
+	                       entries=[list(vars(pokemon).values()) for pokemon in return_all()])
+
+if __name__ == '__main__':
+	# print(vars(Pokemon.from_db(1)).keys())
+
+	app.run()
