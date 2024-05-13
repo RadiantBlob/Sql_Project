852a36f663aa6b5cae9c9cbcce6eecc7d01ca060
Felix Müller <mullerfelix178@gmail.com>
2024-05-13 15:53:37 +0200

    added functions to read and write to a given database

---
+++ util/utilities.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+def read_one(file: str, sql: str) -> ():
+	con = sqlite3.connect(file)
+	cursor = con.cursor()
+	cursor.execute(sql)
+	row = cursor.fetchone()
+	con.close()
+	return row
+
+def read_all(file: str, sql: str) -> [()]:
+	con = sqlite3.connect(file)
+	cursor = con.cursor()
+	cursor.execute(sql)
+	rows = cursor.fetchall()
+	con.close()
+	return rows
+
+def write_all(file: str, sql: str):
+	connection = sqlite3.connect(file)  # Muss vorher angelegt werden.
+	cursor = connection.cursor()
+	cursor.execute(sql)
+	connection.commit()
+	connection.close()
+
+if __name__ == '__main__':
+	pass
+	# print(read_one("../poke.db", "Select * from User"))
