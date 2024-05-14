01e8a8db9f6f9ca347a5aca73d7cb63d9d02b1a7
radiantFelix <mullerfelix178@gmai.com>
2024-05-14 17:11:00 +0200

    added the ability to load_pokemon for User

--- user.py
+++ user.py
@@ -7,6 +7,7 @@ from util.utilities import read_one, read_all, write_all
 
 class User:
 	def __init__(self, username: str, password: str):
+		self.inventory: [Pokemon] = []
 		self.username: str = username
 		self._password: str = password
 		self.p_list: [Pokemon] = []
@@ -25,10 +26,17 @@ class User:
 
 
 	def load_pokemon(self):
-		pass
+		sql = f"Select pokemon, level from User_hat_Pokemon where user='{self.username}'"
+		id_list = read_all("poke.db", sql)
+		for id in id_list:
+			self.inventory.append((Pokemon.from_db(id[0]), id[1]))
+		# for p in self.inventory:
+		# 	p.set_filter("010000000000000")
 
 	def __str__(self):
 		return f"{self.username}"
 
 if __name__ == '__main__':
-	print(User.from_db("admin"))
+	admin = User.from_db("admin")
+	admin.load_pokemon()
+	print(admin.inventory)
