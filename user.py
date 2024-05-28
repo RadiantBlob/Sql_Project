8d85ed4af9b88a8735d398752166fe67f17f1818
CWSRC23U29 <CWSRC23U29@PAEDAGOGIK.SCHULEN.LOCAL>
2024-05-28 17:06:19 +0200

    added team and the ability to move from and to team

--- user.py
+++ user.py
@@ -7,7 +7,8 @@ from util.utilities import read_one, read_all, write_all
 
 class User:
 	def __init__(self, username: str, password: str):
-		self.inventory: [(Pokemon, int)] = []
+		self.inventory: [[Pokemon, int]] = []
+		self.team: list[[Pokemon, int]] = []
 		self.username: str = username
 		self._password: str = password
 		self.p_list: [Pokemon] = []
@@ -27,17 +28,33 @@ class User:
 
 
 	def load_pokemon(self):
-		sql = f"Select pokemon, level from User_hat_Pokemon where user='{self.username}'"
+		self.inventory = []
+		self.team = []
+		sql = f"Select pokemon, level, inteam from User_hat_Pokemon where user='{self.username}'"
 		id_list = read_all("poke.db", sql)
 		for id in id_list:
-			self.inventory.append((Pokemon.from_db(id[0]), id[1]))
+			if bool(id[2]):
+				self.team.append([Pokemon.from_db(id[0]), id[1]])
+			else:
+				self.inventory.append([Pokemon.from_db(id[0]), id[1]])
+
+
 		# for p in self.inventory:
 		# 	p.set_filter("010000000000000")
 
 	def write_pokemon(self, id: int, level: int=1):
-		self.inventory.append((Pokemon.from_db(id), level))
+		self.inventory.append([Pokemon.from_db(id), level])
 		sql = f"Insert into User_hat_Pokemon (user, pokemon, level) values ('{self.username}', '{id}', '{level}')"
 		write_all("poke.db", sql)
+
+	def update_database(self):
+		for pokemon, level in self.inventory:
+			sql = f"Insert into User_hat_Pokemon (user, pokemon, level, inteam) values ('{self.username}', '{id}', '{level}', '0')"
+			write_all("poke.db", sql)
+
+		for pokemon, level in self.team:
+			sql = f"Insert into User_hat_Pokemon (user, pokemon, level, inteam) values ('{self.username}', '{id}', '{level}', '1')"
+			write_all("poke.db", sql)
 
 	def delete_pokemon(self, id: int, level: int):
 		sql = f"Select id from User_hat_Pokemon where user = '{self.username}' and pokemon = '{id}' and level = '{level}'"
@@ -54,6 +71,16 @@ class User:
 		self.inventory = []
 		self.load_pokemon()
 
+	def move_to_team(self, pokemon, level):
+		sql = f"update User_hat_Pokemon set inteam = 1 where id = (SELECT id from User_hat_Pokemon where pokemon = '{pokemon}' and level = '{level}' limit 1) "
+		write_all("poke.db", sql)
+		self.load_pokemon()
+
+	def move_to_inventory(self, pokemon, level):
+		sql = f"update User_hat_Pokemon set inteam = 0 where id = (SELECT id from User_hat_Pokemon where pokemon = '{pokemon}' and level = '{level}' limit 1) "
+		write_all("poke.db", sql)
+		self.load_pokemon()
+
 	def __str__(self):
 		return f"{self.username}"
 
@@ -62,5 +89,7 @@ if __name__ == '__main__':
 	# print(admin.inventory)
 	# admin.write_pokemon(135)
 	# print(admin.inventory)
-	admin.delete_pokemon(135, 1)
+	# admin.delete_pokemon(135, 1)
+	# admin.move_to_inventory(4, 2)
 	print(admin.inventory)
+	print(admin.team)
