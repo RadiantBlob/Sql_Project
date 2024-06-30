48df33c9d7e1fd64281a5c2c4da9d5afc1f9a3de
RadiantBlob <mullerfelix178@gmail.com>
2024-07-01 00:32:57 +0200

    refactored the checkboxes and added the option to style them

--- index.py
+++ index.py
@@ -8,28 +8,31 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    ftr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+    argument_filter = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
     filter_arguments = ['ID', 'Name', 'type1', 'type2', 'Generation', 'Abilities', 'hp', 'speed',
                         'attack', 'defense', 'classification', 'height', 'weight', 'legendary']
 
-    pokemon_list = return_all(f=ftr)
+    pokemon_list = return_all(f=argument_filter)
 
     if request.method == 'POST':
         number = request.form.get('Number')
 
         filter_arg_values = {arg: request.form.get(arg) for arg in filter_arguments}
         filter_values = list(filter_arg_values.values())
-        ftr = [0 if val is None else 1 for val in filter_values]
-        ftr.append(0)
+        argument_filter = [0 if val is None else 1 for val in filter_values]
+        argument_filter.append(0)
+
+        dict_arg_val = dict(zip(filter_arguments, argument_filter)) 
 
         if number != '':
-            pokemon_list = return_filter('pokedex_number', f'{number}', f=ftr)
+            pokemon_list = return_filter('pokedex_number', f'{number}', f=argument_filter)
         else:
-            pokemon_list = return_all(f=ftr)
+            pokemon_list = return_all(f=argument_filter)
 
     return render_template("home.html",
                            lable=pokemon_list[0].get_keys(),
-                           entries=[pokemon.get_values() for pokemon in pokemon_list])
+                           entries=[pokemon.get_values() for pokemon in pokemon_list],
+                           checkboxes=dict_arg_val)
 
 
 @app.route("/pokemon/<int:id>")
